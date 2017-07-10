2e1ed7a9d35274bfd5fda86a0544576b30ec14c9
Naoufal Kadhom <naoufalkadhom@gmail.com>
2017-07-09 22:33:21 -0700

    Trash old project

--- StripeManager.h
+++
@@ -1,9 +0,0 @@
-#import "Stripe.h"
-#import "RCTBridgeModule.h"
-
-@interface StripeManager : NSObject
-+ (void)handlePaymentAuthorizationWithPayment:(NSString *)publishableKey
-                                      payment:(PKPayment *)payment
-                                   completion:(void (^)(PKPaymentAuthorizationStatus))completion
-                                     callback:(RCTResponseSenderBlock)callback;
-@end
