c82a3e902d43d0a112af98b537c0f431114e90ff
Bahaa Jr <geekbahaa1@gmail.com>
2017-08-07 18:58:09 +0200

    Handling Android RN 0.47 breaking change
    [facebook/react-native@ce6fb33](https://github.com/facebook/react-native/commit/ce6fb337a146e6f261f2afb564aa19363774a7a8)

--- packages/react-native-payments/lib/android/src/main/java/com/reactlibrary/ReactNativePaymentsPackage.java
+++ packages/react-native-payments/lib/android/src/main/java/com/reactlibrary/ReactNativePaymentsPackage.java
@@ -16,7 +16,7 @@ public class ReactNativePaymentsPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new ReactNativePaymentsModule(reactContext));
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
