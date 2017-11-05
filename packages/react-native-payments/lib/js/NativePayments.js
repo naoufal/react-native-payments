97cc7bbfade6ffe0c405e36099fc18f909345d4e
Michael Elsdörfer <michael@elsdoerfer.info>
2017-11-05 16:06:50 +0000

    Do not fail if native module is unavailable
    For example, when running in a JavaScript test runner, the native addons do not exist, but a simple import of the module should not fail.

--- packages/react-native-payments/lib/js/NativePayments.js
+++ packages/react-native-payments/lib/js/NativePayments.js
@@ -19,7 +19,7 @@ const NativePayments: {
 } = {
   supportedGateways: IS_ANDROID
     ? ['stripe', 'braintree'] // On Android, Payment Gateways are supported out of the gate.
-    : ReactNativePayments.supportedGateways,
+    : ReactNativePayments ? ReactNativePayments.supportedGateways : [],
 
   canMakePayments(methodData: object) {
     return new Promise((resolve, reject) => {
