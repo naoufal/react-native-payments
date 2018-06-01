7f322a4f2dcc6223c3daa443b3fe4ab06e3c720e
Andrej Badin <contact@andrejbadin.com>
2018-06-02 01:13:20 +0200

    Add multi-button demo

--- packages/react-native-payments/examples/native/App.js
+++ packages/react-native-payments/examples/native/App.js
@@ -14,6 +14,7 @@ type State = {
 export class App extends React.Component<Props, State> {
   state = {
     type: 'donate',
+    type2: 'donate',
     style: 'black',
   };
 
@@ -29,10 +30,18 @@ export class App extends React.Component<Props, State> {
         }}
       >
         <ApplePayPaymentButton
-          onPress={this.onPress}
-          buttonType={this.state.type}
           buttonStyle={this.state.style}
+          buttonType={this.state.type}
+          onPress={this.onPress}
         />
+
+        <View style={{ width: 150, marginVertical: 50 }}>
+          <ApplePayPaymentButton
+            buttonStyle={this.state.style}
+            buttonType={this.state.type}
+            onPress={() => alert('2')}
+          />
+        </View>
 
         <View style={{ padding: 20 }}>
           <View style={{ marginVertical: 50 }}>
@@ -41,6 +50,13 @@ export class App extends React.Component<Props, State> {
             <Text style={{ color: this.state.type === 'setUp' ? 'red': 'black' }} onPress={() => this.setState({ type: 'setUp' })}>setUp</Text>
             <Text style={{ color: this.state.type === 'inStore' ? 'red': 'black' }} onPress={() => this.setState({ type: 'inStore' })}>inStore</Text>
             <Text style={{ color: this.state.type === 'donate' ? 'red': 'black' }} onPress={() => this.setState({ type: 'donate' })}>donate</Text>
+          </View>
+          <View style={{ marginVertical: 50 }}>
+            <Text style={{ color: this.state.type2 === 'plain' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'plain' })}>plain</Text>
+            <Text style={{ color: this.state.type2 === 'buy' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'buy' })}>buy</Text>
+            <Text style={{ color: this.state.type2 === 'setUp' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'setUp' })}>setUp</Text>
+            <Text style={{ color: this.state.type2 === 'inStore' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'inStore' })}>inStore</Text>
+            <Text style={{ color: this.state.type2 === 'donate' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'donate' })}>donate</Text>
           </View>
           <View style={{ marginVertical: 50 }}>
             <Text style={{ color: this.state.style === 'white' ? 'red': 'black' }} onPress={() => this.setState({ style: 'white' })}>white</Text>
