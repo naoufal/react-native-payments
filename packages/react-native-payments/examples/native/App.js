6540126c9af6ea6ceac9e13b10b36aa0d5c9614d
Andrej Badin <contact@andrejbadin.com>
2018-06-03 17:28:23 +0200

    Format

--- packages/react-native-payments/examples/native/App.js
+++ packages/react-native-payments/examples/native/App.js
@@ -2,7 +2,11 @@
 
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { ApplePayPaymentButton, type PKPaymentButtonType, type PKPaymentButtonStyle } from './ApplePayPaymentButton';
+import {
+  ApplePayPaymentButton,
+  type PKPaymentButtonType,
+  type PKPaymentButtonStyle,
+} from './ApplePayPaymentButton';
 
 type Props = {};
 
@@ -35,33 +39,102 @@ export class App extends React.Component<Props, State> {
           onPress={this.onPress}
         />
 
-        <View style={{ width: 150, marginVertical: 50 }}>
+        {/* <View style={{ width: 150, marginVertical: 50 }}>
           <ApplePayPaymentButton
             buttonStyle={this.state.style}
             buttonType={this.state.type}
             onPress={() => alert('2')}
           />
-        </View>
+        </View> */}
 
         <View style={{ padding: 20 }}>
           <View style={{ marginVertical: 50 }}>
-            <Text style={{ color: this.state.type === 'plain' ? 'red': 'black' }} onPress={() => this.setState({ type: 'plain' })}>plain</Text>
-            <Text style={{ color: this.state.type === 'buy' ? 'red': 'black' }} onPress={() => this.setState({ type: 'buy' })}>buy</Text>
-            <Text style={{ color: this.state.type === 'setUp' ? 'red': 'black' }} onPress={() => this.setState({ type: 'setUp' })}>setUp</Text>
-            <Text style={{ color: this.state.type === 'inStore' ? 'red': 'black' }} onPress={() => this.setState({ type: 'inStore' })}>inStore</Text>
-            <Text style={{ color: this.state.type === 'donate' ? 'red': 'black' }} onPress={() => this.setState({ type: 'donate' })}>donate</Text>
+            <Text
+              style={{ color: this.state.type === 'plain' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type: 'plain' })}
+            >
+              plain
+            </Text>
+            <Text
+              style={{ color: this.state.type === 'buy' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type: 'buy' })}
+            >
+              buy
+            </Text>
+            <Text
+              style={{ color: this.state.type === 'setUp' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type: 'setUp' })}
+            >
+              setUp
+            </Text>
+            <Text
+              style={{ color: this.state.type === 'inStore' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type: 'inStore' })}
+            >
+              inStore
+            </Text>
+            <Text
+              style={{ color: this.state.type === 'donate' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type: 'donate' })}
+            >
+              donate
+            </Text>
           </View>
           <View style={{ marginVertical: 50 }}>
-            <Text style={{ color: this.state.type2 === 'plain' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'plain' })}>plain</Text>
-            <Text style={{ color: this.state.type2 === 'buy' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'buy' })}>buy</Text>
-            <Text style={{ color: this.state.type2 === 'setUp' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'setUp' })}>setUp</Text>
-            <Text style={{ color: this.state.type2 === 'inStore' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'inStore' })}>inStore</Text>
-            <Text style={{ color: this.state.type2 === 'donate' ? 'red': 'black' }} onPress={() => this.setState({ type2: 'donate' })}>donate</Text>
+            <Text
+              style={{ color: this.state.type2 === 'plain' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type2: 'plain' })}
+            >
+              plain
+            </Text>
+            <Text
+              style={{ color: this.state.type2 === 'buy' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type2: 'buy' })}
+            >
+              buy
+            </Text>
+            <Text
+              style={{ color: this.state.type2 === 'setUp' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type2: 'setUp' })}
+            >
+              setUp
+            </Text>
+            <Text
+              style={{
+                color: this.state.type2 === 'inStore' ? 'red' : 'black',
+              }}
+              onPress={() => this.setState({ type2: 'inStore' })}
+            >
+              inStore
+            </Text>
+            <Text
+              style={{ color: this.state.type2 === 'donate' ? 'red' : 'black' }}
+              onPress={() => this.setState({ type2: 'donate' })}
+            >
+              donate
+            </Text>
           </View>
           <View style={{ marginVertical: 50 }}>
-            <Text style={{ color: this.state.style === 'white' ? 'red': 'black' }} onPress={() => this.setState({ style: 'white' })}>white</Text>
-            <Text style={{ color: this.state.style === 'whiteOutline' ? 'red': 'black' }} onPress={() => this.setState({ style: 'whiteOutline' })}>whiteOutline</Text>
-            <Text style={{ color: this.state.style === 'black' ? 'red': 'black' }} onPress={() => this.setState({ style: 'black' })}>black</Text>
+            <Text
+              style={{ color: this.state.style === 'white' ? 'red' : 'black' }}
+              onPress={() => this.setState({ style: 'white' })}
+            >
+              white
+            </Text>
+            <Text
+              style={{
+                color: this.state.style === 'whiteOutline' ? 'red' : 'black',
+              }}
+              onPress={() => this.setState({ style: 'whiteOutline' })}
+            >
+              whiteOutline
+            </Text>
+            <Text
+              style={{ color: this.state.style === 'black' ? 'red' : 'black' }}
+              onPress={() => this.setState({ style: 'black' })}
+            >
+              black
+            </Text>
           </View>
         </View>
       </View>
