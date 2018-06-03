7039ad13659abf03b365884a4f5cafa325eca437
Andrej Badin <contact@andrejbadin.com>
2018-06-04 00:31:22 +0200

    Reset example to original state

--- packages/react-native-payments/examples/native/index.ios.js
+++ packages/react-native-payments/examples/native/index.ios.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import { App } from './App';
 global.PaymentRequest = require('react-native-payments').PaymentRequest;
-// const App = require('../common/App').default;
+const App = require('../common/App').default;
 
 AppRegistry.registerComponent('ReactNativePaymentsExample', () => App);
