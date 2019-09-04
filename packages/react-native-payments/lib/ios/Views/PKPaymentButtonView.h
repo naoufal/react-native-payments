95e36223ed0246fadba8ccd6885cdf9ce643cddf
Cameron Bates <cameron@heyimcameron.com>
2019-09-03 20:41:30 -0400

    Add native support for cornerRadius

--- packages/react-native-payments/lib/ios/Views/PKPaymentButtonView.h
+++ packages/react-native-payments/lib/ios/Views/PKPaymentButtonView.h
@@ -13,6 +13,7 @@
 
 @property (strong, nonatomic) NSString *buttonStyle;
 @property (strong, nonatomic) NSString *buttonType;
+@property (nonatomic) CGFloat cornerRadius;
 @property (nonatomic, readonly) PKPaymentButton *button;
 @property (nonatomic, copy) RCTBubblingEventBlock onPress;
 
