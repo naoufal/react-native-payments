cdbf130db7394f382ef7ba00f84861337aaa2df6
Tzvetlin Velev <tvelev@paciolan.com>
2020-02-24 13:59:50 -0800

    remove the examples folder

--- packages/react-native-payments/examples/native/index.ios.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-global.PaymentRequest = require('react-native-payments').PaymentRequest;
-const App = require('../common/App').default;
-
-AppRegistry.registerComponent('ReactNativePaymentsExample', () => App);
