214d409de4f09e805b62f9bd87545da92ce7a9da
Nabil Freeman <nabil@instafork.com>
2020-11-18 13:53:10 +0000

    sync from react-native-easy-payments

---
+++ ios/Views/PKPaymentButtonManager.m
@@ -0,0 +1,45 @@
+//
+//  ApplePayPaymentButtonManager.m
+//  ReactNativePaymentsExample
+//
+//  Created by Andrej on 15/05/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <PassKit/PassKit.h>
+#import "PKPaymentButtonManager.h"
+#import "PKPaymentButtonView.h"
+
+@implementation PKPaymentButtonManager
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_VIEW_PROPERTY(onPress, RCTBubblingEventBlock)
+
+RCT_CUSTOM_VIEW_PROPERTY(buttonType, NSString, PKPaymentButtonView)
+{
+  if (json) {
+    [view setButtonType:[RCTConvert NSString:json]];
+  }
+}
+
+RCT_CUSTOM_VIEW_PROPERTY(buttonStyle, NSString, PKPaymentButtonView)
+{
+  if (json) {
+    [view setButtonStyle:[RCTConvert NSString:json]];
+  }
+}
+
+RCT_CUSTOM_VIEW_PROPERTY(cornerRadius, CGFloat, PKPaymentButtonView)
+{
+  if (json) {
+    [view setCornerRadius:[RCTConvert CGFloat:json]];
+  }
+}
+
+- (UIView *) view
+{
+  return [PKPaymentButtonView new];
+}
+
+@end
---
+++ js/index.js
@@ -0,0 +1,2 @@
+export { default as PaymentRequest } from './PaymentRequest';
+export { PKPaymentButton } from './PKPaymentButton';
