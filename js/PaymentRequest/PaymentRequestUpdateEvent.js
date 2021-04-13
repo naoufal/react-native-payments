296ecb92636ded24b8f5958f0a4903445533692b
nmilcoff <nicolasmilcoff@gmail.com>
2021-04-13 11:32:48 -0300

    Do not trigger a ShippingOption reset if the original event is actually a ShippingOption change (#290)
    * Do not trigger a ShippingOption reset if the original event is actually a ShippingOption change
    
    * Add missing &&

--- js/PaymentRequest/PaymentRequestUpdateEvent.js
+++ js/PaymentRequest/PaymentRequestUpdateEvent.js
@@ -127,7 +127,9 @@ export default class PaymentRequestUpdateEvent {
 
           if (
             target._details.shippingOptions &&
-            target._details.shippingOptions.length > 0
+            target._details.shippingOptions.length > 0 &&
+            value.shippingOptions &&
+            value.shippingOptions.find(op => op.selected)?.id !== target._shippingOption
           ) {
             target._handleShippingOptionChange({
               selectedShippingOptionId: target._details.shippingOptions[0].id
