78a9a1673107484cf3fa98427eed44704839c652
Tyler Coffman <tyler.coffman@appfolio.com>
2021-11-22 11:12:40 -0800

    Revert test changes from PR #1
    We are no longer changing behavior for how PaymentRequest handles its details
    constructor argument, so the test changes from PR #1 are no longer valid. Revert these.
    
    There is a better way of changing PaymentRequestUpdateEvent, so revert all the
    changes from PR #1 and then re-do the tests in a following commit.

--- js/PaymentRequest/__tests__/PaymentRequestUpdateEvent.test.js
+++ js/PaymentRequest/__tests__/PaymentRequestUpdateEvent.test.js
@@ -14,11 +14,8 @@ const PaymentRequestUpdateEvent = require('../PaymentRequestUpdateEvent')
 import {
   createCreatedPaymentRequest,
   createInteractivePaymentRequest,
-  createInteractiveCreditPaymentRequest,
-  createInteractiveDebitPaymentRequest,
   createClosedPaymentRequest,
-  createUpdatingPaymentRequest,
-  APPLE_PAY_DETAILS
+  createUpdatingPaymentRequest
 } from './index.test.js';
 
 // constants
@@ -65,7 +62,7 @@ describe('PaymentRequestUpdateEvent', () => {
   const paymentRequest = new PaymentRequest(METHOD_DATA, DETAILS, OPTIONS);
 
   describe('constructor', () => {
-    it("should throw if `name` isn't `shippingaddresschange` or `shippingoptionchange` or `paymentmethodchange`", () => {
+    it("should throw if `name` isn't `shippingaddresschange` or `shippingoptionchange`", () => {
       expect(() => {
         const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
           'foo',
@@ -91,30 +88,6 @@ describe('PaymentRequestUpdateEvent', () => {
       it('should return the PaymentRequest instance', () => {
         const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
           'shippingaddresschange',
-          paymentRequest
-        );
-
-        expect(paymentRequestUpdateEvent.target).toBeInstanceOf(PaymentRequest);
-      });
-    });
-  });
-
-  describe('attributes', () => {
-    describe('name', () => {
-      it('should return the event name paymentmethodchange', () => {
-        const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
-          'paymentmethodchange',
-          paymentRequest
-        );
-
-        expect(paymentRequestUpdateEvent.name).toBe('paymentmethodchange');
-      });
-    });
-
-    describe('target', () => {
-      it('should return the PaymentRequest instance paymentmethodchange', () => {
-        const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
-          'paymentmethodchange',
           paymentRequest
         );
 
@@ -150,82 +123,6 @@ describe('PaymentRequestUpdateEvent', () => {
         ],
         modifiers: 'foo' // Not sure how these are used yet
       };
-
-      const updateCreditDetails = {
-        id: 'credit_card_details',
-        displayItems: [
-          {
-            label: 'Movie Ticket',
-            amount: {currency: 'USD', value: '15.00'}
-          },
-          {
-            label: 'Fee',
-            amount: {currency: 'USD', value: '3.50'}
-          }
-        ],
-        shippingOptions: [{
-          id: 'economy',
-          label: 'Economy Shipping',
-          amount: {currency: 'USD', value: '0.00'},
-          detail: 'Arrives in 3-5 days' // `detail` is specific to React Native Payments
-        }],
-        total: {
-          label: 'Merchant Name',
-          amount: {currency: 'USD', value: '18.50'},
-        }
-      };
-
-      const updateDebitDetails = {
-        id: 'debit_card_details',
-        displayItems: [
-          {
-            label: 'Movie Ticket',
-            amount: {currency: 'USD', value: '15.00'}
-          },
-          {
-            label: 'Fee',
-            amount: {currency: 'USD', value: '2.99'}
-          }
-        ],
-        shippingOptions: [{
-          id: 'economy',
-          label: 'Economy Shipping',
-          amount: {currency: 'USD', value: '0.00'},
-          detail: 'Arrives in 3-5 days' // `detail` is specific to React Native Payments
-        }],
-        total: {
-          label: 'Merchant Name',
-          amount: {currency: 'USD', value: '17.99'},
-        }
-      };
-
-      const DEFAULT_DETAILS = {
-        id: 'default_details',
-        displayItems: [
-          {
-            label: 'Rent',
-            amount: {currency: 'USD', value: '15.00'}
-          },
-          {
-            label: 'Fee',
-            amount: {currency: 'USD', value: '0.00'}
-          }
-        ],
-        shippingOptions: [{
-          id: 'economy',
-          label: 'Economy Shipping',
-          amount: {currency: 'USD', value: '0.00'},
-          detail: 'Arrives in 3-5 days' // `detail` is specific to React Native Payments
-        }],
-        total: {
-          label: 'Merchant Name',
-          amount: {currency: 'USD', value: '10'},
-        }
-      };
-
-      const updatedCreditDetails = Object.assign({}, DEFAULT_DETAILS, updateCreditDetails);
-      const updatedDebitDetails = Object.assign({}, DEFAULT_DETAILS, updateDebitDetails);
-
       const updatedDetails = Object.assign({}, DETAILS, updateDetails);
 
       it('should throw if `target` is not an instance of PaymentRequest', async () => {
@@ -354,38 +251,6 @@ describe('PaymentRequestUpdateEvent', () => {
         expect(interactivePaymentRequest._details).toEqual(updatedDetails);
       });
 
-      it('should successfully update target details for credit paymentmethodchange when passed an Object', async () => {
-        const interactivePaymentRequest = createInteractiveCreditPaymentRequest(
-          METHOD_DATA,
-          APPLE_PAY_DETAILS,
-          OPTIONS
-        );
-        const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
-          'paymentmethodchange',
-          interactivePaymentRequest
-        );
-
-        await paymentRequestUpdateEvent.updateWith(updateCreditDetails);
-
-        expect(interactivePaymentRequest._details).toEqual(updatedCreditDetails);
-      });
-
-      it('should successfully update target details for debit paymentmethodchange when passed an Object', async () => {
-        const interactivePaymentRequest = createInteractiveDebitPaymentRequest(
-            METHOD_DATA,
-            APPLE_PAY_DETAILS,
-            OPTIONS
-        );
-        const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
-            'paymentmethodchange',
-            interactivePaymentRequest
-        );
-
-        await paymentRequestUpdateEvent.updateWith(updateDebitDetails);
-
-        expect(interactivePaymentRequest._details).toEqual(updatedDebitDetails);
-      });
-
       it('should successfully update target details when passed a Promise that returns an Object', async () => {
         const interactivePaymentRequest = createInteractivePaymentRequest(
           METHOD_DATA,
@@ -402,42 +267,6 @@ describe('PaymentRequestUpdateEvent', () => {
         });
 
         expect(interactivePaymentRequest._details).toEqual(updatedDetails);
-      });
-
-      it('should successfully update target details for credit paymentmethodchange when passed a Promise that returns an Object', async () => {
-        const interactivePaymentRequest = createInteractiveCreditPaymentRequest(
-            METHOD_DATA,
-            APPLE_PAY_DETAILS,
-            OPTIONS
-        );
-        const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
-            'paymentmethodchange',
-            interactivePaymentRequest
-        );
-
-        await paymentRequestUpdateEvent.updateWith(() => {
-          return Promise.resolve(updateCreditDetails);
-        });
-
-        expect(interactivePaymentRequest._details).toEqual(updatedCreditDetails);
-      });
-
-      it('should successfully update target details for debit paymentmethodchange when passed a Promise that returns an Object', async () => {
-        const interactivePaymentRequest = createInteractiveDebitPaymentRequest(
-            METHOD_DATA,
-            APPLE_PAY_DETAILS,
-            OPTIONS
-        );
-        const paymentRequestUpdateEvent = new PaymentRequestUpdateEvent(
-            'paymentmethodchange',
-            interactivePaymentRequest
-        );
-
-        await paymentRequestUpdateEvent.updateWith(() => {
-          return Promise.resolve(updateDebitDetails);
-        });
-
-        expect(interactivePaymentRequest._details).toEqual(updatedDebitDetails);
       });
 
       it('should return a rejected promise upon rejection of the details Promise', async () => {
@@ -460,7 +289,7 @@ describe('PaymentRequestUpdateEvent', () => {
         } catch(e) {
           error = e;
         }
-
+        
         expect(error.message).toBe('Error fetching shipping prices.');
       });
     });
